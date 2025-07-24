67c18ffe4fd63c8621ab7917eff95d13d2b04a49
Greg Gallagher <greg_gallagher@amat.com>
2025-07-24 15:33:03 -0400

    CMSIS: Add file to source cmsis packages
    Signed-off-by: Greg Gallagher <greg_gallagher@amat.com>

---
+++ cmsis_setup.sh
@@ -0,0 +1,5 @@
+export CMSIS_PACK_ROOT=/Volumes/devel/packs
+export CMSIS_COMPILER_ROOT=/Volumes/devel/cmsis-toolbox-darwin-arm64/etc
+export PATH=/Volumes/devel/cmsis-toolbox-darwin-arm64/bin:$PATH
+export GCC_TOOLCHAIN_14_2_1=/Applications/ArmGNUToolchain/14.2.rel1/arm-none-eabi/bin
+. ~/.vcpkg/vcpkg-init
